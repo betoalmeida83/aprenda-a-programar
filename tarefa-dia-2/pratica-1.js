62412b3e00aa60d76fe75410d2999a62ff4b318a
Beto Almeida <roberto.jalmeidafilho@gmail.com>
2022-08-24 08:48:09 -0300

    commit Pratica 1

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,10 @@
+let nome
+nome = "Roberto Almeida"
+let email
+email = "roberto.jalmeidafilho@gmail.com"
+let profissao
+profissao = "Gerente de Vendas"
+let idade
+idade = "39 anos"
+let viagemdosonho
+viagemdosonho = "California"
