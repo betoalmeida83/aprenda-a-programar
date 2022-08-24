dd3c1a25c9241bd00355810b8c4f7b21db16d622
Beto Almeida <roberto.jalmeidafilho@gmail.com>
2022-08-24 10:32:48 -0300

    commit Desafio Opicional

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -13,5 +13,4 @@ console.log(email)
 console.log(profissao)
 console.log(idade)
 console.log(viagemdosonho)
-console.log('Olá, ${nome} ! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${viagemdosonho}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D')
-
+console.log(`Olá, ${nome} ! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${viagemdosonho}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
