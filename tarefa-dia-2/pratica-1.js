972b693b7f6d73314ddcba908ae321c848ff4815
Beto Almeida <roberto.jalmeidafilho@gmail.com>
2022-08-24 19:27:34 -0300

    commit Desafio Opicional

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,16 +1,15 @@
-let nome
-nome = "Roberto Almeida"
-let email
-email = "roberto.jalmeidafilho@gmail.com"
-let profissao
-profissao = "Gerente de Vendas"
-let idade
-idade = "39 anos"
-let viagemdosonho
-viagemdosonho = "California"
+let nome = "Roberto Almeida"
+let email = "roberto.jalmeidafilho@gmail.com"
+let profissao = "Gerente de Vendas"
+let idade = "39 anos"
+let viagemdosonho = "California"
 console.log(nome)
 console.log(email)
 console.log(profissao)
 console.log(idade)
 console.log(viagemdosonho)
-console.log(`Olá, ${nome} ! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${viagemdosonho}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
+let mensagem = `Olá, ${nome} ! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${viagemdosonho}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`
+console.log(mensagem)
+
+
+
