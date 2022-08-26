bcea42c43318c3b0312e60053d0148c1abd7fd55
Beto Almeida <roberto.jalmeidafilho@gmail.com>
2022-08-26 09:35:43 -0300

    commit pratica-3

--- tarefa-dia-2/pratica-3.js
+++ tarefa-dia-2/pratica-3.js
@@ -6,10 +6,6 @@ tamanhocodigo = codigosecreto.length
 
 if (tamanhocodigo > 6110) {
     console.log("Perdão, Alan Turing. Vocês não conseguirão derrotar os nazistas. :'(");
-}
-
-var senha = "12345";
-
-if (tamanhocodigo <= 6110) {
+} else {
     console.log("Maravilha! O código tem menos ou é igual a 6111 caracteres! Mãos à obra! Decifrem o código o quanto antes que vocês vão conseguir derrotá-los! :D");
 }
